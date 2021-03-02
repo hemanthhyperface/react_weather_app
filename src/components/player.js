5f665098ac591468fe660c689f5636151bf3fcc5
hemanth rs <hemanth@dooziesoft.com>
2021-03-02 21:48:19 +0530

    UI fixes

--- src/components/player.js
+++ src/components/player.js
@@ -14,15 +14,19 @@ class Player extends React.Component {
         else {
             return (
 
-
+               
+                
                 <div className="player">
-
-                    <h5 className="text-center pname" >{this.props.value}</h5>
-
-                    <img className="picture" src={this.props.image} alt=""></img>
-                    <p className="text-center bowlstyle">{this.props.result.description} </p>
-
+                {this.props.value ?  <>
+                    <h5 className="text-center text-capitalize text-lg pname" >{this.props.value}</h5>
+                    <img className="picture" height="50" width="50" src={this.props.image} alt=""></img>
+                    <p className="text-center text-capitalize bowlstyle">{this.props.result.description} </p>
+                    </>
+                    : <h4 className="text-center">----</h4>
+                }
                 </div>
+              
+               
 
             )
         }
--- src/components/search.js
+++ src/components/search.js
@@ -4,11 +4,11 @@ class Search extends React.Component {
         return (
             <div className="search">
             <div className="col">
-            <h4> WEATHER SEARCH</h4>
-            <label>enter city name</label>
+            <h4 className="text-center"> WEATHER SEARCH</h4>
+            <label>Enter city name</label>
             <form onSubmit={this.props.submit}>
                <input className="hello form-control"  type="text"  value={this.props.value} onChange={this.props.change}></input> <br/>
-               <button type="submit" className="btn btn-primary">Submit</button>
+               <button type="submit" className="btn btn-primary btn-block">Submit</button>
               </form>
 </div>
             </div>
