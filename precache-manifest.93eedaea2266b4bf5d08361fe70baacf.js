aebf4328d6e4c730175799eb42dc309d54e01e50
hemanth rs <hemanth@dooziesoft.com>
2021-07-04 12:37:59 +0530

    Updates

---
+++ precache-manifest.93eedaea2266b4bf5d08361fe70baacf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1ac8787ea8680f1722814255d73bf9",
+    "url": "/react_weather_app/index.html"
+  },
+  {
+    "revision": "743cf4dcbfb34d70e0e6",
+    "url": "/react_weather_app/static/css/main.cb04eda6.chunk.css"
+  },
+  {
+    "revision": "2f1e94b926f0ee51cc31",
+    "url": "/react_weather_app/static/js/2.3f07041c.chunk.js"
+  },
+  {
+    "revision": "743cf4dcbfb34d70e0e6",
+    "url": "/react_weather_app/static/js/main.c9bcbe99.chunk.js"
+  },
+  {
+    "revision": "94bcb110c597deb45865",
+    "url": "/react_weather_app/static/js/runtime~main.c1dfc44d.js"
+  }
+]);
